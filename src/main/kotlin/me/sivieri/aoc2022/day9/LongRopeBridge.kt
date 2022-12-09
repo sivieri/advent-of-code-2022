72b749b346c8d04ace4d71800718930f31195186
Alessandro Sivieri <alessandro.sivieri@gmail.com>
2022-12-09 15:22:10 +0100

    Day 9 part 2 work

---
+++ src/main/kotlin/me/sivieri/aoc2022/day9/LongRopeBridge.kt
@@ -0,0 +1,67 @@
+package me.sivieri.aoc2022.day9
+
+import me.sivieri.aoc2022.common.Coordinate2D
+import kotlin.math.max
+
+class LongRopeBridge(
+    var head: Coordinate2D = Coordinate2D(0, 0),
+    var tail: Coordinate2D = Coordinate2D(0, 0)
+) {
+
+    val visitedTail: MutableList<Coordinate2D> = mutableListOf(tail.copy())
+
+    init {
+        assert(tail.cellDistance(head) <= 2)
+    }
+
+    fun move(ropeMovement: RopeMovement) {
+        (1..ropeMovement.steps).forEach { _ ->
+            // head
+            head = when (ropeMovement.ropeDirection) {
+                RopeDirection.LEFT -> Coordinate2D(head.x - 1, head.y)
+                RopeDirection.RIGHT -> Coordinate2D(head.x + 1, head.y)
+                RopeDirection.UP -> Coordinate2D(head.x, head.y + 1)
+                RopeDirection.DOWN -> Coordinate2D(head.x, head.y - 1)
+            }
+            // tail
+            when (tail.cellDistance(head)) {
+                0 -> { }
+                1 -> { }
+                2 -> {
+                    tail = tail.moveOneCellTowards(head)
+                    visitedTail.add(tail.copy())
+                }
+                else -> throw IllegalArgumentException("Tail $tail too distant from head $head!")
+            }
+        }
+    }
+
+    fun move(ropeMovements: List<RopeMovement>): Unit = ropeMovements.forEach { move(it) }
+
+    fun countTailPositions(): Int = visitedTail.distinct().size
+
+    companion object {
+        fun Coordinate2D.cellDistance(other: Coordinate2D): Int = when {
+            this.x == other.x -> kotlin.math.abs(this.y - other.y)
+            this.y == other.y -> kotlin.math.abs(this.x - other.x)
+            else -> max(kotlin.math.abs(this.y - other.y), kotlin.math.abs(this.x - other.x))
+        }
+
+        fun Coordinate2D.moveOneCellTowards(other: Coordinate2D): Coordinate2D = when {
+            // vertical distance
+            this.x == other.x && this.y > other.y -> Coordinate2D(this.x, this.y - 1)
+            this.x == other.x && this.y < other.y -> Coordinate2D(this.x, this.y + 1)
+            // horizontal distance
+            this.x > other.x && this.y == other.y -> Coordinate2D(this.x - 1, this.y)
+            this.x < other.x && this.y == other.y -> Coordinate2D(this.x + 1, this.y)
+            // diagonal distance
+            this.x > other.x && this.y > other.y -> Coordinate2D(this.x - 1, this.y - 1)
+            this.x < other.x && this.y < other.y -> Coordinate2D(this.x + 1, this.y + 1)
+            this.x > other.x && this.y < other.y -> Coordinate2D(this.x - 1, this.y + 1)
+            this.x < other.x && this.y > other.y -> Coordinate2D(this.x + 1, this.y - 1)
+            // same place
+            else -> this
+        }
+    }
+
+}
---
+++ src/test/kotlin/me/sivieri/aoc2022/day9/LongRopeBridgeTest.kt
@@ -0,0 +1,44 @@
+package me.sivieri.aoc2022.day9
+
+import me.sivieri.aoc2022.common.Coordinate2D
+import org.hamcrest.MatcherAssert
+import org.hamcrest.Matchers.`is`
+import org.junit.Test
+
+class LongRopeBridgeTest {
+
+    @Test
+    fun `part 2 example 1`() {
+        val input = """
+            R 4
+            U 4
+            L 3
+            D 1
+            R 4
+            D 1
+            L 5
+            R 2
+        """.trimIndent().split("\n").map { RopeMovement.parseLine(it.trim()) }
+        val ropeBridge = LongRopeBridge(Coordinate2D(0, 0), Coordinate2D(0, 0))
+        ropeBridge.move(input)
+        MatcherAssert.assertThat(ropeBridge.countTailPositions(), `is`(1))
+    }
+
+    @Test
+    fun `part 2 example 2`() {
+        val input = """
+            R 5
+            U 8
+            L 8
+            D 3
+            R 17
+            D 10
+            L 25
+            U 20
+        """.trimIndent().split("\n").map { RopeMovement.parseLine(it.trim()) }
+        val ropeBridge = LongRopeBridge(Coordinate2D(0, 0), Coordinate2D(0, 0))
+        ropeBridge.move(input)
+        MatcherAssert.assertThat(ropeBridge.countTailPositions(), `is`(36))
+    }
+
+}
