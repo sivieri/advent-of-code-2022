e1983642b23a5c1051921a016f16d4bd35f7e43e
Alessandro Sivieri <alessandro.sivieri@gmail.com>
2023-09-16 11:21:20 +0200

    Day 20 part 1 not handling duplicates

--- src/main/kotlin/me/sivieri/aoc2022/day20/CoordinatesEncrypter.kt
+++ src/main/kotlin/me/sivieri/aoc2022/day20/CoordinatesEncrypter.kt
@@ -1,56 +1,27 @@
 package me.sivieri.aoc2022.day20
 
+import kotlin.math.abs
+
 class CoordinatesEncrypter {
 
     fun decrypt(data: List<Int>): Int {
         val size = data.size
-        var mixed = data.toTypedArray()
+        val mixed = data.toTypedArray()
         data.forEach { v ->
-            val cur = mixed.copyOf()
-            val index = cur.indexOf(v)
-            val newIndex = getNewIndex(index, size, v)
-            println("Value $v from $index to $newIndex")
-            mixed = Array(size) { 0 }
-            var i = 0
-            cur.forEach { v ->
-                if (v != cur[index]) mixed[i++] = v
+            (0 until abs(v)).forEach { _ ->
+                val index = mixed.indexOf(v)
+                val newIndex = if (v < 0) (index - 1).mod(size) else (index + 1).mod(size)
+                val swap = mixed[newIndex]
+                mixed[newIndex] = mixed[index]
+                mixed[index] = swap
             }
-            mixed = push(mixed.copyOf(), v, newIndex)
-            println(mixed.joinToString(", "))
         }
-        return mixed[getValue(FIRST_INDEX, size)] +
-            mixed[getValue(SECOND_INDEX, size)] +
-            mixed[getValue(THIRD_INDEX, size)]
-    }
-
-    private fun getNewIndex(
-        initialPosition: Int,
-        size: Int,
-        value: Int
-    ): Int {
-        val res = (initialPosition + value) % size
-        return if (res > 0) res
-        else if (res < 0) size + res - 1
-        else size - 1
+        return mixed[getValue(FIRST_INDEX + mixed.indexOf(0), size)] +
+            mixed[getValue(SECOND_INDEX+ mixed.indexOf(0), size)] +
+            mixed[getValue(THIRD_INDEX+ mixed.indexOf(0), size)]
     }
 
     private fun getValue(index: Int, size: Int): Int = index % size
-
-    private fun push(
-        current: Array<Int>,
-        value: Int,
-        index: Int
-    ): Array<Int> {
-        val result = Array(current.size) { 0 }
-        (0 until index).forEach { i ->
-            result[i] = current[i]
-        }
-        result[index] = value
-        ((index + 1) until current.size).forEach { i ->
-            result[i] = current[i - 1]
-        }
-        return result
-    }
 
     companion object {
         private const val FIRST_INDEX = 1000
--- src/main/kotlin/me/sivieri/aoc2022/day20/Main1.kt
+++ src/main/kotlin/me/sivieri/aoc2022/day20/Main1.kt
@@ -1,10 +1,18 @@
 package me.sivieri.aoc2022.day20
 
+import me.sivieri.aoc2022.Utils
+
 object Main1 {
 
     @JvmStatic
     fun main(args: Array<String>) {
-
+        val data = Utils
+            .readInputToList(20)
+            .filter { it.isNotBlank() }
+            .map { it.toInt() }
+        val coordinatesEncrypter = CoordinatesEncrypter()
+        val result = coordinatesEncrypter.decrypt(data)
+        println(result)
     }
 
 }
